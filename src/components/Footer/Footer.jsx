467f16df6f1e34f6db1eae429aafa97a67250add
Alexander Messier <alexander.j.messier@gmail.com>
2021-10-05 15:21:52 -0500

    Changed stlying of the footer, and made it a footer tag instead of a div tag.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import './Footer.css'
+import './Footer.css';
 
-function Footer(props){
-    return (
-        <div className='row'>
-            <div className='col-md-12' style={{ padding: 0 }}>
-                <div className='footer'>
-                    <h5>2021 Alex's Library</h5>
-                </div>
-            </div>
-        </div>
-    )
+const Footer = () => {
+    return ( 
+        <footer className='footer'>
+            <h4>Copyright © 2021</h4>
+        </footer>
+    );
 }
-
+ 
 export default Footer;
