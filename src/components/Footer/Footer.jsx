b879b5bd415d694f0134c5a241c666564ef29671
Alexander Messier <alexander.j.messier@gmail.com>
2021-10-05 13:35:24 -0500

    Created Footer Component file and instantiated it in App.jsx.

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer(props){
+    return (
+        <div className='row'>
+            <div className='col-md-12' style={{ padding: 0 }}>
+                <div className='footer'>
+                    <h5>2021 Alex's Library</h5>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
